990bb408f0f67850a4e1c167f3b42ded88164fb5
Petri Tuomola <petri@tuomola.org>
2019-12-30 22:18:17 +0200

    8.9

--- frontend/src/components/Books.js
+++ frontend/src/components/Books.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { gql } from 'apollo-boost'
+import { useQuery } from '@apollo/react-hooks'
 
+const ALL_BOOKS = gql`
+{
+  allBooks  {
+    title
+    author
+    published
+  }
+}
+`
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
 
   return (
     <div>
